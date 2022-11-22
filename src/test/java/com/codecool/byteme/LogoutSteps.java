f218c9125b76972a673dc1c86de15e3a52c7e18f
Hladgerd <hladgerd@gmail.com>
2022-11-22 22:58:26 +0100

    feat: add logout step definitions

---
+++ src/test/java/com/codecool/byteme/LogoutSteps.java
@@ -0,0 +1,39 @@
+package com.codecool.byteme;
+
+import com.codecool.byteme.pages.FeedPage;
+import com.codecool.byteme.pages.LoginPage;
+import io.cucumber.java.After;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LogoutSteps {
+    private LoginPage loginPage;
+    private FeedPage feedPage;
+
+    @Given("The user is logged in to Byte.me")
+    public void openLogin() {
+        loginPage = new LoginPage();
+        feedPage = new FeedPage();
+        loginPage.openLoginPage();
+        loginPage.login();
+    }
+
+    @When("clicks Logout button")
+    public void logout() {
+        feedPage.logOutCurrentUser();
+    }
+
+    @Then("Login page is visible")
+    public void loginPageIsVisible() {
+        loginPage.reOpenLoginPage();
+        assertTrue(loginPage.isLoginButtonVisible());
+    }
+
+//    @After
+//    public void close() {
+//        loginPage.closeWebDriver();
+//    }
+}
