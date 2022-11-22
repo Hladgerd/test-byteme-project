9306f07fdf30ccda7d3529f9441e93bb0c0a021b
Hladgerd <hladgerd@gmail.com>
2022-11-22 18:39:44 +0100

    refact: format code

--- src/main/java/com/codecool/byteme/pages/ProfilePage.java
+++ src/main/java/com/codecool/byteme/pages/ProfilePage.java
@@ -2,15 +2,13 @@ package com.codecool.byteme.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ProfilePage extends BasePage {
     @FindBy(xpath = "//*[@id=\"root\"]/div/div[2]/div[1]/div[2]/div[1]")
     WebElement fullName;
 
-    public String getFullName(){
-        String[] result = fullName.getText().split(" ",2);
+    public String getFullName() {
+        String[] result = fullName.getText().split(" ", 2);
         return result.length > 1 ? result[1] : "";
     }
-
 }
