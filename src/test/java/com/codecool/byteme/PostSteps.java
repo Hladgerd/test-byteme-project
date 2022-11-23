a0d64fea9a662ded5990467f46a36826b000e7fb
Hladgerd <hladgerd@gmail.com>
2022-11-23 23:20:04 +0100

    feat: add delete post steps

--- src/test/java/com/codecool/byteme/PostSteps.java
+++ src/test/java/com/codecool/byteme/PostSteps.java
@@ -10,45 +10,71 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class PostSteps {
     private LoginPage loginPage;
     private FeedPage feedPage;
     private Boolean shouldBeDeleted;
+    private String uniqueTitle;
+    private String uniqueBody;
 
     @Before
     public void init() {
         loginPage = new LoginPage();
         feedPage = new FeedPage();
-        loginPage.openLoginPage();
         loginPage.login();
         shouldBeDeleted = false;
     }
 
     @Given("the user is on Feed page")
     public void openFeedPage() {
-        loginPage.openFeedPage();
+        feedPage.openFeeds();
     }
 
-    @When("fills up post {string} and {string} and clicks Add button")
-    public void fillsUpForm(String title, String body) {
-        feedPage.createNewPost(title, body);
+    @When("fills up post title and body and clicks Add button")
+    public void fillsUpForm() {
+        uniqueTitle = Util.generateRandomString();
+        uniqueBody = Util.generateRandomString();
+        feedPage.createNewPost(uniqueTitle, uniqueBody);
         shouldBeDeleted = true;
     }
 
-    @Then("the given {string} equals the title of the latest post")
-    public void checkTitle(String title) {
-        assertEquals(title, feedPage.getLatestPostTitle());
+    @Then("the given title equals the title of the latest post")
+    public void checkTitleEquals() {
+        assertEquals(uniqueTitle, feedPage.getNewlyCreatedPostTitle(uniqueTitle));
     }
 
-    @And("the given {string} equals the body of the latest post")
-    public void checkBody(String body) {
-        assertEquals(body, feedPage.getLatestPostBody());
+    @And("the given body equals the body of the latest post")
+    public void checkBodyEquals() {
+        assertEquals(uniqueBody, feedPage.getNewlyCreatedPostBody(uniqueBody));
+    }
+
+    @Given("a new post has been created")
+    public void post() {
+        uniqueTitle = Util.generateRandomString();
+        uniqueBody = Util.generateRandomString();
+        feedPage.createNewPost(uniqueTitle, uniqueBody);
+    }
+
+    @When("user deletes the new post")
+    public void deletePost() {
+        feedPage.deleteNewPost(uniqueTitle);
+    }
+
+    @Then("the created post title is different from last post's title")
+    public void checkTitleDiffers() {
+        assertNotEquals(uniqueTitle, feedPage.getLatestPostTitle());
+    }
+
+    @And("the created post body is different from last post's body")
+    public void checkBodyDiffers() {
+        assertNotEquals(uniqueBody, feedPage.getLatestPostBody());
     }
 
     @After
     public void close() {
-        if(shouldBeDeleted) feedPage.deleteNewPost();
+        if(shouldBeDeleted) feedPage.deleteNewPost(uniqueTitle);
         loginPage.closeWebDriver();
     }
 }
